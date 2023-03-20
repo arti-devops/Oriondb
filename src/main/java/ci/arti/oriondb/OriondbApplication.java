cbdb5a4a1cd4a6a49ccd67e03c38231c5f9934c6
Al-Karid <grencisse@hotmail.com>
2023-03-20 17:01:19 +0000

    Initial commit

---
+++ src/main/java/ci/arti/oriondb/OriondbApplication.java
@@ -0,0 +1,13 @@
+package ci.arti.oriondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriondbApplication.class, args);
+	}
+
+}
