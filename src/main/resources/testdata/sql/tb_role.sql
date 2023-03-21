4e04a5f6bb9005a80cf93a8b9d0dee0230bb3660
Al-Karid <grencisse@hotmail.com>
2023-03-21 11:27:58 +0000

    Add test data

---
+++ src/main/resources/testdata/sql/tb_role.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_role (`role_id`,`name`,`short_name`) VALUES (1,'DIRECTEUR','ROLE-DIR'),
+	(2,'SOUS-DIRECTEUR','ROLE-SDIR'),
+	(3,'CHEF DE SERVICE','ROLE-CS'),
+	(4,'CHARGE D''ETUDE','ROLE-CE'),
+	(5,'AGENT','ROLE-AG');
