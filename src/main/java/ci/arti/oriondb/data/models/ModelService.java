2647a8117038bb71f30c28d49cfe66f002a79d8f
Al-Karid <grencisse@hotmail.com>
2023-03-21 09:55:30 +0000

    Update ModelService.java

--- src/main/java/ci/arti/oriondb/data/models/ModelService.java
+++ src/main/java/ci/arti/oriondb/data/models/ModelService.java
@@ -1,9 +1,14 @@
 package ci.arti.oriondb.data.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "service")
+@Getter @Setter @NoArgsConstructor
 public class ModelService {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
