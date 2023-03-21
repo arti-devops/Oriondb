e776ef328794a2c6f44dfcb4962e4af64cfa9843
Al-Karid <grencisse@hotmail.com>
2023-03-21 18:09:10 +0000

    Update, filter absence by positionID and date range

--- src/main/java/ci/arti/oriondb/controllers/checkin/ControllerAbsence.java
+++ src/main/java/ci/arti/oriondb/controllers/checkin/ControllerAbsence.java
@@ -1,14 +1,17 @@
 package ci.arti.oriondb.controllers.checkin;
 
 import ci.arti.oriondb.data.models.checkin.ModelAbsence;
+import ci.arti.oriondb.data.models.employee.ModelPosition;
 import ci.arti.oriondb.data.repository.checkin.RepositoryAbsence;
 import ci.arti.oriondb.exception.ResourceNotFoundException;
+import ci.arti.oriondb.services.ServicePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -18,11 +21,28 @@ public class ControllerAbsence {
     @Autowired
     private RepositoryAbsence repositoryAbsence;
 
+    @Autowired
+    private ServicePosition servicePosition;
+
     @GetMapping("/all")
     private List<ModelAbsence> getAllAbsences(){return repositoryAbsence.findAll();}
 
     @GetMapping("/{id}")
     private ModelAbsence getAbsenceById(@PathVariable Long id){
         return repositoryAbsence.findById(id).orElseThrow(() -> new ResourceNotFoundException("Absence","ID",id));
+    }
+
+    @GetMapping("/{s}/{e}")
+    private List<ModelAbsence> getAllByAbsDateBetween(@PathVariable Date s,
+                                                      @PathVariable Date e){
+        return repositoryAbsence.findAllByAbsDateBetween(s,e);
+    }
+
+    @GetMapping("/{p}/{s}/{e}")
+    private List<ModelAbsence> getAllByPositionAndAbsDateBetween(@PathVariable Long p,
+                                                                 @PathVariable Date e,
+                                                                 @PathVariable Date s){
+        ModelPosition position = servicePosition.getASinglePosition(p);
+        return repositoryAbsence.findAllByPositionAndAbsDateBetween(position, s, e);
     }
 }
