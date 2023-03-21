16b8aba7ba1f502bff09c48eb017e8711bcb9f0c
Al-Karid <grencisse@hotmail.com>
2023-03-21 17:50:56 +0000

    Update, filter logdata by positionID and date range

--- src/main/java/ci/arti/oriondb/controllers/checkin/ControllerLogbook.java
+++ src/main/java/ci/arti/oriondb/controllers/checkin/ControllerLogbook.java
@@ -1,8 +1,10 @@
 package ci.arti.oriondb.controllers.checkin;
 
 import ci.arti.oriondb.data.models.checkin.ModelLogbook;
+import ci.arti.oriondb.data.models.employee.ModelPosition;
 import ci.arti.oriondb.data.repository.checkin.RepositoryLogbook;
 import ci.arti.oriondb.exception.ResourceNotFoundException;
+import ci.arti.oriondb.services.ServicePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,9 @@ public class ControllerLogbook {
     @Autowired
     private RepositoryLogbook repositoryLogbook;
 
+    @Autowired
+    ServicePosition servicePosition;
+
     @GetMapping("/all")
     private List<ModelLogbook> getAllLogs(){return repositoryLogbook.findAll();}
 
@@ -28,7 +33,16 @@ public class ControllerLogbook {
     }
 
     @GetMapping("/{s}/{e}")
-    private List<ModelLogbook> getLogBetween(@PathVariable Date s, @PathVariable Date e){
+    private List<ModelLogbook> getLogBetween(@PathVariable Date s,
+                                             @PathVariable Date e){
         return repositoryLogbook.findModelLogbookByLogDateBetween(s,e);
+    }
+
+    @GetMapping("/{p}/{s}/{e}")
+    private List<ModelLogbook> getPositionLogBetween(@PathVariable Long p,
+                                                     @PathVariable Date s,
+                                                     @PathVariable Date e){
+        ModelPosition position = servicePosition.getASinglePosition(p);
+        return repositoryLogbook.findModelLogbookByPositionAndLogDateBetween(position,s,e);
     }
 }
--- src/main/java/ci/arti/oriondb/data/repository/checkin/RepositoryLogbook.java
+++ src/main/java/ci/arti/oriondb/data/repository/checkin/RepositoryLogbook.java
@@ -1,6 +1,7 @@
 package ci.arti.oriondb.data.repository.checkin;
 
 import ci.arti.oriondb.data.models.checkin.ModelLogbook;
+import ci.arti.oriondb.data.models.employee.ModelPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.Date;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface RepositoryLogbook extends JpaRepository<ModelLogbook, Long> {
     List<ModelLogbook> findModelLogbookByLogDateBetween(Date startDate, Date endDate);
+    List<ModelLogbook> findModelLogbookByPositionAndLogDateBetween(ModelPosition position, Date startDate, Date endDate);
 }
