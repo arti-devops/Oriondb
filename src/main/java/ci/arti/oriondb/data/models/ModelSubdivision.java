8e79c514b366534f44d7f1c21bfafbbd2b687f48
Al-Karid <grencisse@hotmail.com>
2023-03-21 09:46:45 +0000

    Create ModelSubdivision.java

---
+++ src/main/java/ci/arti/oriondb/data/models/ModelSubdivision.java
@@ -0,0 +1,35 @@
+package ci.arti.oriondb.data.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "subdivision")
+@Getter @Setter @NoArgsConstructor
+public class ModelSubdivision {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "subdivision_id")
+    private Integer subdivisionId;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String shortName;
+
+    @ManyToOne
+    @JoinColumn(name = "subdivision_id", nullable = true, insertable = false, updatable = false)
+    private ModelSubdivision direction;
+
+    @OneToMany(mappedBy = "direction", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<ModelSubdivision> subdivisions = new ArrayList<>();
+}
