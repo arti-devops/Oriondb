301e661b107fe50ae2033290009beb07ba575315
Al-Karid <grencisse@hotmail.com>
2023-03-21 09:55:42 +0000

    Update ModelRole.java

--- src/main/java/ci/arti/oriondb/data/models/ModelRole.java
+++ src/main/java/ci/arti/oriondb/data/models/ModelRole.java
@@ -1,9 +1,14 @@
 package ci.arti.oriondb.data.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "role")
+@Getter @Setter @NoArgsConstructor
 public class ModelRole {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roleId;
