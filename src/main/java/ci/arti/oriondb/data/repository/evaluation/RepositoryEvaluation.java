f935dfc93909eaab1d394eb4c6f224618859ed86
Al-Karid <grencisse@hotmail.com>
2023-04-13 23:22:26 +0000

    Update Evaluation, load data by position id

--- src/main/java/ci/arti/oriondb/data/repository/evaluation/RepositoryEvaluation.java
+++ src/main/java/ci/arti/oriondb/data/repository/evaluation/RepositoryEvaluation.java
@@ -1,7 +1,11 @@
 package ci.arti.oriondb.data.repository.evaluation;
 
+import ci.arti.oriondb.data.models.employee.ModelPosition;
 import ci.arti.oriondb.data.models.evaluation.ModelEvaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepositoryEvaluation extends JpaRepository<ModelEvaluation, Long> {
+    List<ModelEvaluation> findAllByPosition(ModelPosition p);
 }
